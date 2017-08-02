1979fc84cd90d0b1bb839e7d7f718885ce27e680
Ben Scheirman <ben@scheirman.com>
2017-08-02 14:07:23 -0500

    Episode 296

---
+++ 3. RectCapture-preview-layer-sample-buffer/RectCapture/CaptureViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  RectCapture
+//
+//  Created by Ben Scheirman on 6/27/17.
+//  Copyright © 2017 NSScreencast. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CoreImage
+
+class CaptureViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = .high
+        return session
+    }()
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    let sampleBufferQueue = DispatchQueue.global(qos: .userInteractive)
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if AVCaptureDevice.authorizationStatus(for: .video) == .authorized {
+            setupCaptureSession()
+        } else {
+            AVCaptureDevice.requestAccess(for: .video, completionHandler: { (authorized) in
+                DispatchQueue.main.async {
+                    if authorized {
+                        self.setupCaptureSession()
+                    }
+                }
+            })
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer?.bounds = view.frame
+    }
+    
+    // MARK: - Rotation
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return [.portrait]
+    }
+    
+    // MARK: - Camera Capture
+    
+    private func findCamera() -> AVCaptureDevice? {
+        let deviceTypes: [AVCaptureDevice.DeviceType] = [
+            .builtInDualCamera,
+            .builtInTelephotoCamera,
+            .builtInWideAngleCamera
+        ]
+        
+        let discovery = AVCaptureDevice.DiscoverySession(deviceTypes: deviceTypes,
+                                                         mediaType: .video,
+                                                         position: .back)
+        
+        return discovery.devices.first
+    }
+    
+    private func setupCaptureSession() {
+        guard captureSession.inputs.isEmpty else { return }
+        guard let camera = findCamera() else {
+            print("No camera found")
+            return
+        }
+        
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            captureSession.addInput(cameraInput)
+            
+            let preview = AVCaptureVideoPreviewLayer(session: captureSession)
+            preview.frame = view.bounds
+            preview.backgroundColor = UIColor.black.cgColor
+            preview.videoGravity = .resizeAspect
+            view.layer.addSublayer(preview)
+            self.previewLayer = preview
+            
+            let output = AVCaptureVideoDataOutput()
+            output.alwaysDiscardsLateVideoFrames = true
+            output.setSampleBufferDelegate(self, queue: sampleBufferQueue)
+            
+            captureSession.addOutput(output)
+            
+            captureSession.startRunning()
+            
+        } catch let e {
+            print("Error creating capture session: \(e)")
+            return
+        }
+    }
+    
+}
+
+extension CaptureViewController : AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("sampleBuffer")
+    }
+}
